152d2004a9415e94bffdb9b7c5c3cca152e903c7
Tulio <tulio.pereirab@gmail.com>
2020-09-25 12:19:58 -0300

    LUT generator script modified to create only the file needed. The path for the scripts was changed, hence the script Modelsim was also changed.

--- Scripts/lut-generator.py
+++ Scripts/lut-generator.py
@@ -1,5 +1,6 @@
 BINARY_WIDTH = 16       # change this value will change the number of bits generated in each memory position
-
+mif_mem = 2             # define the type of file to be generated
+                        # 1- for .mif; 2- for .mem
 
 def padded_bin(i, width):
     s = "{0:b}".format(i)
@@ -7,24 +8,24 @@ def padded_bin(i, width):
 
 def mif_creation(lut):
     if (lut == 1):  # lut_u
-        file = open("lut_u.mif", "w")
+        file = open("../lut/lut_u.mif", "w")
     else:
-        file = open("lut_v.mif", "w")
+        file = open("../lut/lut_v.mif", "w")
     file.write("WIDTH=8;\nDEPTH=256;\n\nADDRESS_RADIX=UNS.\nDATA_RADIX=BIN;\n\nCONTENT BEGIN\n")
     file.close()
 
 def mem_creation(lut):              # just add the numbers sequentialy
     if (lut == 1):  # lut_u
-        file = open("lut_u.mem", "w")
+        file = open("../lut/lut_u.mem", "w")
     else:
-        file = open("lut_v.mem", "w")
+        file = open("../lut/lut_v.mem", "w")
     file.close()
 
 def mif_insertion(lut, index, bin):
     if (lut == 1):  # lut_u
-        file = open("lut_u.mif", "a")
+        file = open("../lut/lut_u.mif", "a")
     else:
-        file = open("lut_v.mif", "a")
+        file = open("../lut/lut_v.mif", "a")
     file.write("\t" + str(index) + "\t:\t" + bin + ";\n")
     if(index == 255):
         file.write("END;\n")
@@ -32,16 +33,18 @@ def mif_insertion(lut, index, bin):
 
 def mem_insertion(lut, hex_value):
     if (lut == 1):  # lut_u
-        file = open("lut_u.mem", "a")
+        file = open("../lut/lut_u.mem", "a")
     else:
-        file = open("lut_v.mem", "a")
+        file = open("../lut/lut_v.mem", "a")
     file.write(hex_value + "\n")
     file.close()
 
-def lut_u():
+def lut_u(def_file):
     count = -1
-    mem_creation(1)
-    mif_creation(1)        # creating file U
+    if(def_file == 1):
+        mif_creation(1)        # creating file U
+    else:
+        mem_creation(1)
     for N in range(0,16):
         for s in range(0,16):
             value = 4 * (N - (s - 1))
@@ -50,14 +53,22 @@ def lut_u():
             bin_value = padded_bin(value, BINARY_WIDTH)
             hex_value = format(value, 'x')
             count += 1
-            print(str(N) + "," + str(s) + " -> " + str(value) + " - " + bin_value)
-            mem_insertion(1, hex_value)
-            mif_insertion(1, count, bin_value)
+            #print(str(N) + "," + str(s) + " -> " + str(value) + " - " + bin_value)
+            if(def_file == 1):
+                mif_insertion(1, count, bin_value)
+            else:
+                mem_insertion(1, hex_value)
+    if(def_file == 1):
+        print("Mif file U created.")
+    else:
+        print("Mem file U created.")
 
-def lut_v():
+def lut_v(def_file):
     count = -1
-    mem_creation(2)
-    mif_creation(2)        # creating file V
+    if(def_file == 1):
+        mif_creation(2)        # creating file V
+    else:
+        mem_creation(2)
     for N in range(0,16):
         for s in range(0,16):
             value = 4 * (N - (s + 0))
@@ -66,9 +77,15 @@ def lut_v():
             bin_value = padded_bin(value, BINARY_WIDTH)
             hex_value = format(value, 'x')
             count += 1
-            print("-> " + str(count) + " " + str(N) + "," + str(s) + " -> " + str(value) + " - " + bin_value)
-            mem_insertion(2, hex_value)
-            mif_insertion(2, count, bin_value)
+            #print("-> " + str(count) + " " + str(N) + "," + str(s) + " -> " + str(value) + " - " + bin_value)
+            if(def_file == 1):
+                mif_insertion(2, count, bin_value)
+            else:
+                mem_insertion(2, hex_value)
+    if(def_file == 1):
+        print("Mif file V created.")
+    else:
+        print("Mem file V created.")
 
-lut_u()
-lut_v()
+lut_u(mif_mem)
+lut_v(mif_mem)
--- verification_area/simple_sv/re-run.do
+++ verification_area/simple_sv/re-run.do
@@ -1,8 +1,8 @@
 restart -f
 
 # load memories
-mem load -i C:/Users/Tulio/Desktop/arithmetic_encoder_av1/Scripts/lut_u.mem -format hex /tb_arith_encoder/arith_encoder/state_pipeline_1/lut_u/rom
-mem load -i C:/Users/Tulio/Desktop/arithmetic_encoder_av1/Scripts/lut_v.mem -format hex /tb_arith_encoder/arith_encoder/state_pipeline_1/lut_v/rom
+mem load -i C:/Users/Tulio/Desktop/arithmetic_encoder_av1/lut/lut_u.mem -format hex /tb_arith_encoder/arith_encoder/state_pipeline_1/lut_u/rom
+mem load -i C:/Users/Tulio/Desktop/arithmetic_encoder_av1/lut/lut_v.mem -format hex /tb_arith_encoder/arith_encoder/state_pipeline_1/lut_v/rom
 
 
 # do wave.do
