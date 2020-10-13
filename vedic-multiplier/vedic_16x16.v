c67b54098dff55af290373fc95099842ee5d9703
Tulio <tulio.pereirab@gmail.com>
2020-10-12 22:12:54 -0300

    Created the vedic 16x16 multiplier. I made a 10 million randomic inputs test and it worked perfectly. This module isn't in the arithmetic encoder yet.

---
+++ vedic-multiplier/vedic_16x16.v
@@ -0,0 +1,64 @@
+// This architecture, vedic_8x8 and vedic_4x4 were based on the following article:
+    // @INPROCEEDINGS{7100233,
+    //       author={U. {Narula} and R. {Tripathi} and G. {Wakhle}},
+    //       booktitle={2015 2nd International Conference on Computing for Sustainable Global Development (INDIACom)},
+    //       title={High speed 16-bit digital Vedic multiplier using FPGA},
+    //       year={2015},
+    //       volume={},
+    //       number={},
+    //       pages={121-124},
+    // }
+
+module vedic_16x16 (
+    input [15:0] a, b,
+    output [31:0] r
+    );
+
+    wire [15:0] vedic_1_out, vedic_2_out, vedic_3_out, vedic_4_out;
+    wire [15:0] out_ripple_1, out_ripple_2, out_ripple_3;
+    wire carry_ripple_1, carry_ripple_2, carry_ripple_3;
+
+
+    // first line
+    vedic_8x8 vedic_8x8_1 (
+        .a (a[15:8]),
+        .b (b[15:8]),
+        .r (vedic_1_out)
+        );
+
+    vedic_8x8 vedic_8x8_2 (
+        .a (a[7:0]),
+        .b (b[15:8]),
+        .r (vedic_2_out)
+        );
+
+    vedic_8x8 vedic_8x8_3 (
+        .a (a[15:8]),
+        .b (b[7:0]),
+        .r (vedic_3_out)
+        );
+
+    vedic_8x8 vedic_8x8_4 (
+        .a (a[7:0]),
+        .b (b[7:0]),
+        .r (vedic_4_out)
+        );
+
+    // ripple carries
+    wire [15:0] lsb1_msb4;
+
+    assign lsb1_msb4 = {vedic_1_out[7:0], vedic_4_out[15:8]};
+
+
+    // it is supposed to use RIPPLE ADDER, BUT I'LL USE NORMAL ADD FOR NOW
+    wire [19:0] adder_out;
+    assign adder_out = lsb1_msb4 + vedic_2_out + vedic_3_out;
+
+
+
+    // final
+    assign r[7:0] = vedic_4_out[7:0];
+    assign r[24:8] = adder_out[15:0];
+    assign r[31:24] = vedic_1_out[15:8] + adder_out[19:16];
+    //assign r[7] = carry_ripple_3;
+endmodule
--- vedic-multiplier/vedic_2x2.v
+++ vedic-multiplier/vedic_2x2.v
@@ -1,3 +1,40 @@
+// This module was based on the following article:
+
+    // @INPROCEEDINGS{7034180,
+    //     author={Y. B. {Prasad} and G. {Chokkakula} and P. S. {Reddy} and N. R. {Samhitha}},
+    //     booktitle={International Conference on Information Communication and Embedded Systems (ICICES2014)},
+    //     title={Design of low power and high speed modified carry select adder for 16 bit Vedic Multiplier},
+    //     year={2014},
+    //     volume={},
+    //     number={},
+    //     pages={1-6},
+    //     doi={10.1109/ICICES.2014.7034180}
+    // }
+
+// -----------------------------
+// Some other articles talking about the Vedic multiplier are:
+//@INPROCEEDINGS{8554743,
+//  author={S. {Sujina} and R. {Remya}},
+//  booktitle={2018 International Conference on Advances in Computing, Communications and Informatics (ICACCI)},
+//  title={An Effective Method For Hardware Multiplication Using Vedic Mathematics},
+//  year={2018},
+//  volume={},
+//  number={},
+//  pages={1499-1504},
+//  doi={10.1109/ICACCI.2018.8554743}
+//}
+
+// @INPROCEEDINGS{5423043,
+    // author={M. E. {Paramasivam} and R. S. {Sabeenian}},
+    // booktitle={2010 IEEE 2nd International Advance Computing Conference (IACC)},
+    // title={An efficient bit reduction binary multiplication algorithm using vedic methods},
+    // year={2010},
+    // volume={},
+    // number={},
+    // pages={25-28},
+    // doi={10.1109/IADCC.2010.5423043}
+// }
+
 module vedic_2x2 (
     input [1:0] a, b,
     output wire [3:0] r
