8f6636978491bd054c415e68bc553a1b33aff94e
Tulio Bitencourt <tulio.pereirab@gmail.com>
2020-10-29 13:42:08 -0300

    Arithmetic encoder working properly and creating the pre-bitstream file and the final bitstream file in order to facilitate further validation.

--- verification_area/c-arithmetic-encoder-av1/arith_encoder.c
+++ verification_area/c-arithmetic-encoder-av1/arith_encoder.c
@@ -13,11 +13,12 @@
 #define EC_MIN_PROB 4
 #define CDF_PROB_TOP 32768
 
-#define MAX_INPUTS 10000000
+#define MAX_INPUTS 2000000000
 
 int16_t cnt;
 uint16_t range;
 uint32_t low;
+int offs = 0;
 
 uint32_t get_Low();
 uint16_t get_Range();
@@ -26,6 +27,7 @@ void od_ec_encode_q15(unsigned fl, unsigned fh, int s, int nsyms);;
 void od_ec_encode_bool_q15(int val, unsigned f);
 void od_ec_enc_normalize(uint32_t low_norm, unsigned rng);
 void add_bitstream_file(uint16_t bitstream);
+void carry_propagation();
 
 uint32_t get_Low(){
      return low;
@@ -84,18 +86,21 @@ int main(){
                //printf("Output:\nLow = %" PRIu32 "\tRange = %" PRIu16 "\tCnt = %" PRId16 "\n----------------------\n", low, range, cnt);
                i++;
           }
+          if(status == 0){
+               printf("\n=========================\nExecution finished with error\n");
+               printf("Line: %i\n\t-> Low expected: %" PRIu32 ", got %" PRIu32 "\n\t-> Range expected: %" PRIu16 ", got %" PRIu16 "\n", i-1, file_output_low, low, file_output_range, range);
+               printf("--------------------------------------------------\n");
+               return 0;
+          }else if(reset == 1){
+               printf("=========================\nFinished with reset\n");
+          }else{
+               printf("\n=========================\nNo error found.\n");
+          }
      }else{
           printf("Unable to open the input file.\n");
+          return 0;
      }
-    if(status == 0){
-        printf("\n=========================\nExecution finished with error\n");
-        printf("Line: %i\n\t-> Low expected: %" PRIu32 ", got %" PRIu32 "\n\t-> Range expected: %" PRIu16 ", got %" PRIu16 "\n", i-1, file_output_low, low, file_output_range, range);
-        printf("--------------------------------------------------\n");
-     }else if(reset == 1){
-          printf("=========================\nFinished with reset\n");
-     }else{
-          printf("\n=========================\nNo error found.\n");
-     }
+     carry_propagation();
      return 0;
 }
 
@@ -174,14 +179,16 @@ void od_ec_enc_normalize(uint32_t low_norm, unsigned rng) {
           if (s >= 8) {
                // assert(offs < storage);
                // buf[offs++] = (uint16_t)(low >> c);
-               add_bitstream_file((uint16_t)(low >> c));
+               offs++;
+               add_bitstream_file((uint16_t)(low_norm >> c));
                low_norm &= m;
                c -= 8;
                m >>= 8;
           }
           // assert(offs < storage);
           // buf[offs++] = (uint16_t)(low >> c);
-          add_bitstream_file((uint16_t)(low >> c));
+          offs++;
+          add_bitstream_file((uint16_t)(low_norm >> c));
           s = c + d - 24;
           low_norm &= m;
           // enc->offs = offs;
@@ -195,9 +202,79 @@ void od_ec_enc_normalize(uint32_t low_norm, unsigned rng) {
 void add_bitstream_file(uint16_t bitstream){
     FILE *arq;
     if((arq = fopen("output-files/pre_bitstream.csv", "a+")) != NULL){
-         //printf("Adding bitstream: %" PRIu16 "\n", bitstream);
+        //printf("\nAdding bitstream: %" PRIu16 "\n", bitstream);
         fprintf(arq, "%" PRIu16 "\n", bitstream);
         fclose(arq);
     }else
         printf("Unable to open the bitstream file\n");
+}
+
+void carry_propagation(){
+     FILE *arq, *arq_reference;
+     uint16_t temp_bitstream;
+     uint16_t *buf;
+     unsigned char *out;
+     uint32_t l, e, m;
+     int c, s, start = 1, counter = 0, out_size, i;
+     l = low;
+     c = cnt;
+     s = 10;
+     m = 0x3FFF;
+     e = ((l + m) & ~m) | (m + 1);
+     s += c;
+     if(s > 0){
+          unsigned n;
+          if((arq = fopen("output-files/pre_bitstream.csv", "a+")) != NULL){
+               n = (1 << (c + 16)) - 1;
+               do{
+                    temp_bitstream = (uint16_t)(e >> (c + 16));
+                    offs++;
+                    fprintf(arq, "%" PRIu16 "\n", temp_bitstream);
+                    e &= n;
+                    s -= 8;
+                    c -= 8;
+                    n >>= 8;
+               } while(s > 0);
+               fclose(arq);
+          }else{
+               printf("Unable to open the bitstream file.\n");
+               exit(EXIT_FAILURE);
+          }
+     }
+     if((arq = fopen("output-files/pre_bitstream.csv", "r")) != NULL){
+          buf = (uint16_t *)malloc(sizeof(uint16_t));
+          counter++;
+          while((fscanf(arq, "%" SCNd16 "\n", &buf[counter-1])) != EOF){
+               counter++;
+               buf = (uint16_t *)realloc(buf, sizeof(uint16_t) * counter);
+          }
+          counter--;
+     }else{
+          printf("Unable to open the bitstream file.\n");
+          exit(EXIT_FAILURE);
+     }
+
+     // carry propagation itself
+     out_size = offs;
+     out = (unsigned char*)malloc(sizeof(unsigned char) * out_size);
+     c = 0;
+     while(offs > 0){
+          offs--;
+          c = buf[offs] + c;
+          //printf("%" PRIu16 "\n", buf[offs]);
+          out[offs] = (unsigned char)c;
+          c >>= 8;
+     }
+
+
+     if((arq = fopen("output-files/final_bitstream.csv", "w+")) != NULL){
+          for(i=0; i<out_size; i++){
+               fprintf(arq, "%u\n", out[i]);
+          }
+          fclose(arq);
+     }else{
+          printf("Unable to open the final bitstream file.\n");
+          exit(EXIT_FAILURE);
+     }
+
 }
