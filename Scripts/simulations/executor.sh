97b8138bd1a79730ab2b9e446b55951b0ad7b20c
Tulio <tulio.pereirab@gmail.com>
2021-09-27 09:25:29 -0300

    Created the 4-bool testbench and edited the scripts to compile the 4-bool version.

--- Scripts/simulations/executor.sh
+++ Scripts/simulations/executor.sh
@@ -4,6 +4,7 @@ LIST_LP="lists/list_entropy_encoder-lp.txt"
 LIST_1BOOL="lists/list_entropy_encoder_1-bool.txt"
 LIST_2BOOL="lists/list_entropy_encoder_2-bool.txt"
 LIST_3BOOL="lists/list_entropy_encoder_3-bool.txt"
+LIST_4BOOL="lists/list_entropy_encoder_4-bool.txt"
 
 OUTPUT_DIR="outputs/"
 OUTPUT_ORIGINAL="design.out"
@@ -102,7 +103,7 @@ sim_entropy_encoder_2_bool () {
     # echo "Trying to simulate the original"
     # vvp $OUTPUT_DIR/$OUTPUT_ORIGINAL
     echo -e $line_space
-    echo -e "${boldGreen}Successfully compiled: ${underGreen}Improved version.${nc}"
+    echo -e "${boldGreen}Successfully compiled: ${underGreen}Improved (2-bool) version.${nc}"
     # echo -e "${green}Done.${nc}"
   else
     echo -e $line_space
@@ -123,7 +124,28 @@ sim_entropy_encoder_3_bool () {
     # echo "Trying to simulate the original"
     # vvp $OUTPUT_DIR/$OUTPUT_ORIGINAL
     echo -e $line_space
-    echo -e "${boldGreen}Successfully compiled: ${underGreen}Improved version.${nc}"
+    echo -e "${boldGreen}Successfully compiled: ${underGreen}Improved (3-bool) version.${nc}"
+    # echo -e "${green}Done.${nc}"
+  else
+    echo -e $line_space
+    echo -e "${boldRed}Unable to compile. ${red}Check the problems on the log above.${nc}"
+  fi
+}
+
+sim_entropy_encoder_4_bool () {
+  if [ -f "$OUTPUT_DIR/$OUTPUT_IMPROVED" ]
+  then
+    rm $OUTPUT_DIR/$OUTPUT_IMPROVED
+  fi
+
+  echo -e "${cyan}Running Entropy Encoder: ${underCyan}Improved version (3-bool)${nc}"
+  iverilog -o $OUTPUT_DIR/$OUTPUT_IMPROVED -c $LIST_4BOOL
+  if [ -f "$OUTPUT_DIR/$OUTPUT_IMPROVED" ]
+  then
+    # echo "Trying to simulate the original"
+    # vvp $OUTPUT_DIR/$OUTPUT_ORIGINAL
+    echo -e $line_space
+    echo -e "${boldGreen}Successfully compiled: ${underGreen}Improved (4-bool) version.${nc}"
     # echo -e "${green}Done.${nc}"
   else
     echo -e $line_space
@@ -154,4 +176,7 @@ then
 elif [ $1 = "5" ]
 then
   sim_entropy_encoder_1_bool
+elif [ $1 = "6" ]
+then
+  sim_entropy_encoder_4_bool
 fi
