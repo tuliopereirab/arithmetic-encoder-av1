40b91c8c6266d97e3cc0493b995dc221de8de5b3
Tulio <tulio.pereirab@gmail.com>
2021-09-28 12:51:02 -0300

    Updated the modelsim_flow to easily set the dataset and architecture version. Also added some Modelsim .do scripts to work with the modelsim_flow.

---
+++ verification_area/Modelsim/DUT_ent_enc/start_dut_2-bool.do
@@ -0,0 +1,41 @@
+vsim -gui -t ns work.entropy_encoder_tb
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate -divider Inputs
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_clk
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_reset
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_flag_first
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_final_flag
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_fl
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_fh
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_symbol_1
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_symbol_2
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_nsyms
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_bool_1
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/top_bool_2
+add wave -noupdate -divider Outputs
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_FLAG_BITSTREAM
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_BIT_1
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_BIT_2
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_BIT_3
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_BIT_4
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_BIT_5
+add wave -noupdate -radix unsigned /entropy_encoder_tb/DUT_ent_enc/OUT_FLAG_LAST
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {0 ns} 0}
+quietly wave cursor active 0
+configure wave -namecolwidth 150
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {0 ns} {1 us}
--- verification_area/Modelsim/modelsim_flow.tcl
+++ verification_area/Modelsim/modelsim_flow.tcl
@@ -4,11 +4,10 @@
      # for each round of execution, a different dataset is moved into this target directory
 
 # Configurations
-set dump_vcd 1
-set datasets_path "/home/datasets/Reduced_Datasets"
-set cqs [list cq20 cq32 cq43 cq55]
-set configs [list allintra good]
-set videos [list boat_hdr_amazon_720p dark720p_120f Netflix_RollerCoaster_1280x720_60fps_8bit_420_60f Netflix_DrivingPOV_1280x720_60fps_8bit_420_60f KristenAndSara_1280x720_60_120f]
+# set_dataset options: "mercat" or "objective"
+set arc_version 2
+set set_dataset "mercat"
+set dump_vcd 0
 set final_bitstream "bitstream.csv"
 set final_main "main_data.csv"
 
@@ -23,9 +22,34 @@ set target_name "target"
 # Counter
 set counter 0
 
-# Start the simulation
-do DUT_ent_enc/start_dut.do
+# Start the right simulation according to Arc Version
+if { $arc_version == 1 } {
+    puts "Set architecture version 1-bool."
+    do DUT_ent_enc/start_dut_1-bool.do
+} elseif { $arc_version == 2 } {
+    puts "Set architecture version 2-bool."
+    do DUT_ent_enc/start_dut_2-bool.do
+} elseif { $arc_version == 3 } {
+    puts "Set architecture version 3-bool."
+    do DUT_ent_enc/start_dut_3-bool.do
+} else {
+    puts "Set architecture version 4-bool."
+    do DUT_ent_enc/start_dut_4-bool.do
+}
 
+if {$set_dataset == "mercat"} {
+    puts "Using Mercat dataset"
+    set datasets_path "/media/tulio/y4m_files/generated_files"
+    set cqs "Reduced_Datasets"
+    set configs [list cq20 cq55]
+    set videos [list Beauty_1920x1080_120fps_420_8bit_YUV Bosphorus_1920x1080_120fps_420_8bit_YUV HoneyBee_1920x1080_120fps_420_8bit_YUV Jockey_1920x1080_120fps_420_8bit_YUV ReadySetGo_3840x2160_120fps_420_10bit_YUV YachtRide_3840x2160_120fps_420_10bit_YUV]
+} else {
+    puts "Using Objective-2 dataset"
+    set datasets_path "/media/tulio/objective-2/Reduced_Datasets"
+    set cqs [list cq20 cq32 cq43 cq55]
+    set configs [list allintra good]
+    set videos [list boat_hdr_amazon_720p dark720p_120f Netflix_RollerCoaster_1280x720_60fps_8bit_420_60f Netflix_DrivingPOV_1280x720_60fps_8bit_420_60f KristenAndSara_1280x720_60_120f]
+}
 foreach cq $cqs {
      foreach config $configs {
           foreach video $videos {
